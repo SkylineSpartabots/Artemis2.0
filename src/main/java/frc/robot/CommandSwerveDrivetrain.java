c101cb1e4853d08c1109d92811bbd4acee338bf0
Yanda Bao <yandabaodav@gmail.com>
2024-02-04 17:17:38 -0800

    integrated old constants file and cleaned up OOP

--- src/main/java/frc/robot/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/CommandSwerveDrivetrain.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.generated.TunerConstants;
 
 /**
  * Class that extends the Phoenix SwerveDrivetrain class and implements subsystem
@@ -21,6 +22,16 @@ public class CommandSwerveDrivetrain extends SwerveDrivetrain implements Subsyst
     private static final double kSimLoopPeriod = 0.005; // 5 ms
     private Notifier m_simNotifier = null;
     private double m_lastSimTime;
+
+    private static CommandSwerveDrivetrain s_Swerve;
+
+    public static CommandSwerveDrivetrain getInstance(){
+        if(s_Swerve == null){
+            s_Swerve = new CommandSwerveDrivetrain(TunerConstants.DrivetrainConstants, TunerConstants.FrontLeft,
+            TunerConstants.FrontRight, TunerConstants.BackLeft, TunerConstants.BackRight);
+        }
+        return s_Swerve;
+    }
 
     public CommandSwerveDrivetrain(SwerveDrivetrainConstants driveTrainConstants, double OdometryUpdateFrequency, SwerveModuleConstants... modules) {
         super(driveTrainConstants, OdometryUpdateFrequency, modules); //look here for parent library methods
---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,76 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+
+import com.ctre.phoenix6.signals.SensorDirectionValue;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+
+import com.revrobotics.CANSparkBase.IdleMode;
+
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+  public static final int timeOutMs = 10;
+  public static final double stickDeadband = 0.15;
+  public static final double triggerDeadzone = 0.2;
+
+  // hardware ports for all hardware components on the robot
+  // these include CAN IDs, pneumatic hub ports, etc. 
+
+  public static final class hardwarePIDs{
+    public static final double shooterkP = 0.5;
+    public static final double shooterkI = 0.00;
+    public static final double shooterkD = 0.00;
+
+    public static final double pivotkP = 0.5;
+    public static final double pivotkI = 0.00;
+    public static final double pivotkD = 0.00;
+  }
+  public static final class HardwarePorts {
+    // motors (predicted) IDs not fixed
+    public static final int shooterLeaderMotor = 22;
+    public static final int shooterFollowerMotor = 23;
+    public static final int intakeMotor = 24;
+    public static final int climbLeaderMotor = 3;
+    public static final int climbFollowerMotor = 4;
+    public static final int pivotMotor = 30;
+    public static final int pivotCANcoderID = 31;
+
+  }
+
+    /* Mechanism Current Limits */
+  public static final int intakeContinuousCurrentLimit = 30;
+  public static final int intakePeakCurrentLimit = 70;
+  public static final int shooterContinuousCurrentLimit = 30;
+  public static final int shooterPeakCurrentLimit = 70;
+  public static final int pivotContinuousCurrentLimit = 30;
+  public static final int pivotPeakCurrentLimit = 60;
+
+  public static final double FIELD_WIDTH_METERS = 8.21055;
+  public static final double FIELD_LENGTH_METERS = 16.54175;
+
+  // public static final IdleMode intakeNeutralMode = IdleMode.kCoast;
+  // public static final IdleMode shooterNeutralMode = IdleMode.kBrake;
+
+  public static final double noteIntakeDistance = 70.0;
+
+  public static final IdleMode intakeNeutralMode = IdleMode.kCoast;
+  public static final IdleMode shooterNeutralMode = IdleMode.kBrake;
+
+  public static class OperatorConstants {
+    public static final int kDriverControllerPort = 0;
+  }
+}
