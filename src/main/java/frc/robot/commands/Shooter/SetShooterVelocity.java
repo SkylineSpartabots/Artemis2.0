5c81ae5ce2d1de969a9e93bb4572a932d3f7a67a
Darkle <120344717+Dark-IsHere@users.noreply.github.com>
2024-02-23 20:15:11 -0800

    feed fo wart (no tuning)
    sysid

--- src/main/java/frc/robot/commands/Shooter/SetShooterVelocity.java
+++ src/main/java/frc/robot/commands/Shooter/SetShooterVelocity.java
@@ -1,6 +1,7 @@
 package frc.robot.commands.Shooter;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Shooter;
 
@@ -8,8 +9,10 @@ public class SetShooterVelocity extends Command {
     Shooter s_Shooter;
     Double desiredVelocity;
 
-    PIDController topShooterController = new PIDController(0.05, 0.0, 0.0);
-    PIDController botShooterController = new PIDController(0.05, 0.0, 0.0);
+    SimpleMotorFeedforward feedforwardTop = new SimpleMotorFeedforward(0,0,0); 
+    SimpleMotorFeedforward feedforwardBottom = new SimpleMotorFeedforward(0,0,0); 
+    PIDController topShooterController = new PIDController(0.15, 0.01, 0.05);
+    PIDController botShooterController = new PIDController(0.15, 0.01, 0.05);
 
     public SetShooterVelocity(double desiredVelocity) {
         s_Shooter = Shooter.getInstance();
@@ -20,16 +23,17 @@ public class SetShooterVelocity extends Command {
 
     @Override
     public void initialize() {
-        s_Shooter.setTopSetpoint(desiredVelocity);
-        s_Shooter.setBotSetpoint(desiredVelocity);
     }
 
     @Override
     public void execute() {
+        double feedForwardTopVolts = feedforwardTop.calculate(desiredVelocity);
+        double feedForwardBottomVolts = feedforwardBottom.calculate(desiredVelocity);
+
         double topVoltage = topShooterController.calculate(s_Shooter.getBothSpeeds()[0], desiredVelocity);
         double botVoltage = botShooterController.calculate(s_Shooter.getBothSpeeds()[1], desiredVelocity);
-        s_Shooter.setTopVoltage(topVoltage);
-        s_Shooter.setBotVoltage(botVoltage);
+        s_Shooter.setTopVoltage(topVoltage + feedForwardTopVolts);
+        s_Shooter.setBotVoltage(botVoltage + feedForwardBottomVolts);
     }
 
 
