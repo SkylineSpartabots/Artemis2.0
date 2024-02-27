f8cbd2cf291a9566872a3459c1d425b060e0d0e2
Darkle <120344717+Dark-IsHere@users.noreply.github.com>
2024-02-26 19:13:18 -0800

    Align robot off of x,y & math
    Is it joever

--- src/main/java/frc/robot/commands/AutoAlignDrive/PIDAlign.java
+++ src/main/java/frc/robot/commands/AutoAlignDrive/PIDAlign.java
@@ -8,12 +8,15 @@ import java.beans.Visibility;
 
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.Vision;
+
+import org.opencv.core.Point;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
 
@@ -23,16 +26,21 @@ public class PIDAlign extends Command {
   private final Vision s_Vision;
   private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric();
   PIDController alignPID = new PIDController(0, 0, 0); //TODO: tune this
+  
 
   private double currentYaw;
   private double desiredYaw;
-  private PhotonTrackedTarget targetTag;
+  Point desiredPoint;
+  double offsetYaw;
 
-  public PIDAlign() {
+
+  public PIDAlign(Point desiredPoint) {
     s_Vision = Vision.getInstance();
     s_Swerve = CommandSwerveDrivetrain.getInstance();
 
-    addRequirements(s_Vision); //always do add requirements first before calling subsystems
+    this.desiredPoint = desiredPoint;
+
+    addRequirements(s_Vision);
     addRequirements(s_Swerve);
 
   }
@@ -41,25 +49,29 @@ public class PIDAlign extends Command {
   public void initialize() {
     alignPID.reset();
 
-    targetTag = s_Vision.getBestTarget();
+    Pose2d pose = s_Swerve.getPoseByOdometry();
+    Point currentLocation = new Point(pose.getTranslation().getX() , pose.getTranslation().getY());
+    Point translatedPoint = new Point(desiredPoint.x - currentLocation.x , desiredPoint.y - currentLocation.y); // thanks Dave gloobert
 
-    desiredYaw = Math.toRadians(targetTag.getYaw()); //Im hoping .getYaw gives the value in degrees
+    offsetYaw = Math.toRadians(Math.tanh(translatedPoint.y/translatedPoint.x));
   }
 
   @Override
   public void execute() {
 
+    Pose2d pose = s_Swerve.getPoseByOdometry();
+
     try {
       s_Swerve.updateOdometryByVision(); //since you're supposed to have vision target, reset odometry using kalman first
-      currentYaw = s_Swerve.getPoseByOdometry().getRotation().getRadians(); //grab the "accurate" odometry
+      currentYaw = pose.getRotation().getRadians(); //grab the "accurate" odometry
     } catch (Exception e) {}
+
     
+    desiredYaw = offsetYaw + currentYaw;
 
     double rotationSpeed = alignPID.calculate(currentYaw, desiredYaw);
 
-    if (targetTag.getFiducialId() == Constants.Vision.AprilTags.redSpeakerCenter || targetTag.getFiducialId() == Constants.Vision.AprilTags.blueSpeakerCenter){
-      s_Swerve.applyRequest(() -> drive.withRotationalRate(rotationSpeed));
-    }
+    s_Swerve.applyRequest(() -> drive.withRotationalRate(rotationSpeed));
   }
 
   @Override
