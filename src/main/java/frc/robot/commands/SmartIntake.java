f656541268089188e74d57281ec925a404a183e3
Yanda Bao <yandabaodav@gmail.com>
2024-02-29 17:40:37 -0800

    import issue?

--- src/main/java/frc/robot/commands/SmartIntake.java
+++ src/main/java/frc/robot/commands/SmartIntake.java
@@ -6,6 +6,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.commands.Intake.SetIntake;
 import frc.robot.commands.Pivot.SetPivot;
 import frc.robot.subsystems.Indexer;
 import frc.robot.subsystems.Intake;
