50fe4a7997f1a3d0e77e4319b842f62e32d5b6d4
nat-tri1 <natiey1@outlook.com>
2024-03-02 15:15:26 -0800

    horizontal test route

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -259,6 +259,15 @@ public final class Autos {
       
   }
 
+  public static Command Horizontal() {
+     ArrayList<ChoreoTrajectory> trajectory = Choreo.getTrajectoryGroup("Horizontal");
+
+     return new SequentialCommandGroup(
+      FollowChoreoTrajectory(trajectory.get(0)),
+      FollowChoreoTrajectory(trajectory.get(1))
+     );
+  }
+
 
 
   /*
@@ -274,7 +283,8 @@ public final class Autos {
       ThreeNoteFarSide("ThreeNoteFarSide", ThreeNoteFarSide()),
       FourNoteCloseSide("FourNoteCloseSide", FourNoteCloseSide()),
       FourNoteSubwoofer("FourNoteSubwoofer", FourNoteSubwoofer()),
-      TwoNoteSubwoofer("TwoNoteSuboofer", TwoNote());
+      TwoNoteSubwoofer("TwoNoteSuboofer", TwoNote()),
+      Horizontal("Horizontal", Horizontal());
 
       String name;
       Command autoCommand;
