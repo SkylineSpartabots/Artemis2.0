6d045d5dfed7165c5497d67220f95fe62f71fc02
Darkle <120344717+Dark-IsHere@users.noreply.github.com>
2024-03-02 12:45:44 -0800

    like align rotate

--- src/main/java/frc/robot/subsystems/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -77,7 +77,10 @@ public class Climb extends SubsystemBase {
   public void setVoltage(double voltage) {
     climbMotor.setVoltage(voltage);
   }
-  
+  public void setPercentOutput(double percent){
+    climbMotor.set(percent);
+  }
+
   public double getSetPoint() {
     return setState;
   }
