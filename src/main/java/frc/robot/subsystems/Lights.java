75b71ee1ca7d2489bc26f19300563ba6bb2f264c
Elliot HH <eharhar06@proton.me>
2024-04-06 20:05:23 -0700

    off timer and condensing overloaded functions

--- src/main/java/frc/robot/subsystems/Lights.java
+++ src/main/java/frc/robot/subsystems/Lights.java
@@ -1,20 +1,27 @@
 package frc.robot.subsystems;
 
-import javax.management.timer.Timer;
+import edu.wpi.first.wpilibj.Timer;
 
 import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Lights extends SubsystemBase {
     private static Lights instance;
-    private static boolean ran = false;
+
     private int selected = 0;
     private String binaryString;
+
     private final DigitalOutput pin2;
     private final DigitalOutput pin3;
     private final DigitalOutput pin4;
     private final DigitalOutput pin5;
-    private int count = 0;
+
+    private Timer timer;
+    private final int offTime = 5; //Time to wait until the LEDs should be turned off
+    // private boolean ran = false;
+    // private boolean done = false;
+
+    // private int count = 0; // Only used to cycle selected for testing
 
     public static Lights getInstance() {
         if (instance == null) {
@@ -26,10 +33,10 @@ public class Lights extends SubsystemBase {
     public enum ledModes { // TODO decide what each value should be and sync with other side code
         OFF(0),
         RED(1),
-        ShooterAtSpeed(2), // Shooter At Speed
-        ShooterRamping(3), // Shooter Ramp
-        IntakeSuccess(4), // Intake Success
-        Intaking(5), // Intaking
+        ShooterAtSpeed(2), // Shooter At Speed - Orange Solid
+        ShooterRamping(3), // Shooter Ramp - Orange Flash Inequal On/Off
+        IntakeSuccess(4), // Intake Success - Green Solid
+        Intaking(5), // Intaking - Green Flash Equal On/Off
         PURPLE(6),
         PINK(7),
         WHITE(8),
@@ -47,6 +54,8 @@ public class Lights extends SubsystemBase {
     }
 
     public Lights() {
+        timer = new Timer();
+
         pin2 = new DigitalOutput(2);
         pin3 = new DigitalOutput(3);
         pin4 = new DigitalOutput(4);
@@ -55,20 +64,12 @@ public class Lights extends SubsystemBase {
 
     public void setLEDs(ledModes mode) {
         this.selected = mode.getModeNum();
-
-        System.out.println("Selected LED Mode: " + selected);
-
-        // Convert to binary
-        StringBuilder string = new StringBuilder(Integer.toBinaryString(selected));
-        while (string.length() < 4) { // Make sure that the string is 4 bits
-            string.insert(0, "0");
-        }
-        binaryString = string.toString();
-        System.out.println(binaryString);
-        setDigitalOutPins(binaryString);
+        setLEDs(selected);
     }
 
     public void setLEDs(int selected) {
+        timer.start();
+        timer.reset();
         this.selected = selected;
 
         System.out.println("Selected LED Mode: " + selected);
@@ -96,17 +97,32 @@ public class Lights extends SubsystemBase {
 
     @Override
     public void periodic() {
-        count++;
 
-        if (count >= 100) {
-            count = 0;
-            if (selected < 8) {
-                selected++;
-            } else {
-                selected = 0;
-            }
-            setLEDs(selected);
+        //This Block of code cycles every selected mode
+        // count++;
+
+        // if (count >= 100) {
+        // count = 0;
+        // if (selected < 8) {
+        // selected++;
+        // } else {
+        // selected = 0;
+        // }
+        // setLEDs(selected);
+        // }
+
+        //This one simulates a command setting LEDs once and will turn them off after 5 sec
+        // if (!ran) {
+        //     setLEDs(ledModes.ShooterRamping);
+        //     ran = true;
+        // }
+        // Just keep this below part basically the above is the command simulation
+        if (timer.get() > offTime) {
+            setLEDs(ledModes.OFF);
+            // done = true;
         }
+
+        // System.out.println(done);
 
         // System.out.println("PINK");
         // System.out.println("else");
