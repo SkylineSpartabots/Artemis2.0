9951eb6aea17b7315a8c942371f0295dbc0ca783
eggliot <eharhar06@proton.me>
2024-08-26 16:55:05 -0700

    cleaning shooter up

--- src/main/java/frc/robot/commands/Shooter/ZeroShooter.java
+++ src/main/java/frc/robot/commands/Shooter/ZeroShooter.java
@@ -19,7 +19,7 @@ public class ZeroShooter extends Command {
   @Override
   public void initialize() {
     s_Shooter.setTopVelocity(-2);
-    s_Shooter.setBotVelocity(-2);
+    s_Shooter.setBottomVelocity(-2);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -30,13 +30,13 @@ public class ZeroShooter extends Command {
   @Override
   public void end(boolean interrupted) {
     s_Shooter.setTopVelocity(0);
-    s_Shooter.setBotVelocity(0);
+    s_Shooter.setBottomVelocity(0);
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    double speeds[] = s_Shooter.getBothSpeeds();
+    double speeds[] = s_Shooter.getBothVelocities();
     return speeds[0] < 3 && speeds[1] < 3;
   }
 }
