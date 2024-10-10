1e3a680bf2ccd038c9dcb00460ddc043e071158d
Darkle <120344717+Dark-IsHere@users.noreply.github.com>
2024-10-09 20:43:09 -0700

    Clean up

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -50,7 +50,6 @@ public class Vision extends SubsystemBase {
         new Rotation3d(Units.degreesToRadians(0),Units.degreesToRadians(40),Units.degreesToRadians(0))); //center cam
 
     public static AprilTagFieldLayout aprilTagFieldLayout = AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
-//    private static PhotonCamera visionCamera;
 
     private Vision() {
         centerCamera = new PhotonCamera(Constants.Vision.centerCameraName);
@@ -66,107 +65,13 @@ public class Vision extends SubsystemBase {
         return cameraResult;
     }
 
-    // public List<PhotonTrackedTarget> getTargets(boolean isBackLeft) {
-    //    if (isBackLeft) {
-    //         return backLeftCameraResult.getTargets();
-    //     } else {
-    //         return backRightCameraResult.getTargets();
-    //     }
-    // }
     public List<PhotonTrackedTarget> getTargets(){
         return cameraResult.getTargets();
     }
 
-    public CameraResult hasValidTarget() {
+    public Boolean hasValidTarget() {
         PhotonTrackedTarget target = cameraResult.getBestTarget();
-        PhotonTrackedTarget rightTarget = backRightCameraResult.getBestTarget();
-        PhotonTrackedTarget leftTarget = backLeftCameraResult.getBestTarget();
-
-        Boolean centerHasTarget = cameraResult.hasTargets() && target.getFiducialId() >= 1 && target.getFiducialId() <= Constants.Vision.aprilTagMax && target.getPoseAmbiguity() < 0.2 && target.getPoseAmbiguity() > -1;
-        // Boolean backRightHasTarget = backRightCameraResult.hasTargets() && backRightCameraResult.getBestTarget().getFiducialId() >= 1 && backRightCameraResult.getBestTarget().getFiducialId() <= Constants.Vision.aprilTagMax;
-        // Boolean backLeftHasTarget = backLeftCameraResult.hasTargets() && backLeftCameraResult.getBestTarget().getFiducialId() >= 1 && backLeftCameraResult.getBestTarget().getFiducialId() <= Constants.Vision.aprilTagMax;
-
-        if(centerHasTarget){
-            return cameraResult.CENTER;
-        } else {
-            return null;
-        }
-        //  if (backLeftHasTarget && !backRightHasTarget) {
-        //     return CameraResult.BACK_LEFT;
-        // } else if (!backLeftHasTarget && backRightHasTarget) {
-        //     return CameraResult.BACK_RIGHT;
-        // } else if (backLeftHasTarget && backRightHasTarget) {
-        //     return CameraResult.BOTH;
-        // } else {
-        //     return null; 
-        // }
-    }
-
-    // TODO verify that by the end of auto we have lastValidTarget set
-
-    public PhotonTrackedTarget getBestTarget() {
-        CameraResult valids = hasValidTarget();
-        if (valids != null) {
-            lastValidTarget = cameraResult.getBestTarget();
-            // if (valids == CameraResult.BACK_LEFT) { lastValidTarget = backLeftCameraResult.getBestTarget();}
-            // else if (valids == CameraResult.BACK_RIGHT) { lastValidTarget = backRightCameraResult.getBestTarget();}
-            // else if (valids == CameraResult.BOTH) {
-            //     if (backLeftCameraResult.getTimestampSeconds() >= backRightCameraResult.getTimestampSeconds()) { // If both cams have a target get the MOST recent one
-            //         lastValidTarget = backRightCameraResult.getBestTarget();
-            //     } else {
-            //         lastValidTarget = backLeftCameraResult.getBestTarget();
-            //     }
-            // }
-        } 
-        return lastValidTarget;
-    } 
-
-    public double getYaw() {
-        if (getBestTarget() != null) {
-            return getBestTarget().getYaw();
-        }
-        return -1;
-    }
-
-    public double getPitch() {
-        if (getBestTarget() != null) {
-            return getBestTarget().getPitch();
-        }
-        return -1;
-    }
-
-    public double getFloorDistance(){
-        // PhotonTrackedTarget target = getBestTarget();
-        PhotonTrackedTarget target = getCenterTarget();
-
-        if (target != null) {
-            // code for back right corner camera
-            // targetDistance = PhotonUtils.calculateDistanceToTargetMeters(
-            // Units.inchesToMeters(9.1), 
-            // aprilTagFieldLayout.getTagPose(target.getFiducialId()).get().getZ(), 
-            // Units.degreesToRadians(30), 
-            // Units.degreesToRadians(target.getPitch()));
-            targetDistance = PhotonUtils.calculateDistanceToTargetMeters(
-                Constants.Vision.centerCameraHeight, 
-                aprilTagFieldLayout.getTagPose(target.getFiducialId()).get().getZ(), 
-                Constants.Vision.centerCameraPitch, 
-                Units.degreesToRadians(target.getPitch())
-            );
-            floorDistance = targetDistance;
-            return targetDistance;
-        }
-        return -1;
-
-    }
-
-    public boolean hasSpeakerTag(){
-        List<PhotonTrackedTarget> targets = centerCamera.getLatestResult().getTargets();
-        for(PhotonTrackedTarget a : targets){
-            if(a.getFiducialId() == 4 || a.getFiducialId() == 8){
-                return true;
-            }
-        }
-        return false;
+        return cameraResult.hasTargets() && target.getFiducialId() >= 1 && target.getFiducialId() <= Constants.Vision.aprilTagMax && target.getPoseAmbiguity() < 0.2 && target.getPoseAmbiguity() > -1;
     }
 
     public double getDistanceToPose(Pose2d pose){
@@ -220,23 +125,16 @@ public class Vision extends SubsystemBase {
                         PhotonTrackedTarget bestTarget = cameraResult.getBestTarget();
                         Pose3d targetPose = aprilTagFieldLayout.getTagPose(bestTarget.getFiducialId()).orElse(null);
                         // s_Swerve.updateOdometryByVision(PhotonUtils.estimateFieldToRobotAprilTag(aprilTagFieldLayout.getTagPose(bestTarget.getFiducialId())));
-                        // ill fix this like tmr
+                        // ill fix this like
                     } else { System.out.println("Vision failed: no targets");}
                     
                 }
             } else { System.out.println("Vision skip"); }
         }
 
-        // PhotonTrackedTarget bestTarget = getBestTarget();
-        // if(bestTarget == null){
-        //     throw new Exception("No vision target");
-        // } else {
-        //     Pose3d targetPose = aprilTagFieldLayout.getTagPose(bestTarget.getFiducialId()).orElse(null);
-        //     return PhotonUtils.estimateFieldToRobotAprilTag(aprilTagFieldLayout.getTagPose(bestTarget.getFiducialId()));
-    
-
     @Override
     public void periodic() {
+
         updateAprilTagResults();
         if(!cameraResult.targets.isEmpty()) {
             try {
@@ -244,22 +142,8 @@ public class Vision extends SubsystemBase {
             } catch (Exception e){}
         }
 
-
-        Logger.recordOutput("has target", hasValidTarget() != null);
-        Logger.recordOutput("Vision/TargetYaw", getYaw());
-        Logger.recordOutput("Vision/TargetPitch", getPitch());
-        Logger.recordOutput("Vision/FloorDistance", getFloorDistance());
-        Logger.recordOutput("Vision/BestTargetID", getBestTarget() == null ? -1 : getBestTarget().getFiducialId());
-        Logger.recordOutput("Vision/BestTargetAmbiguity", getBestTarget() != null ? getBestTarget().getPoseAmbiguity() : -1);
-
-        SmartDashboard.putBoolean("has target", hasValidTarget() != null);
-        SmartDashboard.putNumber("Target yaw", getYaw());
-        SmartDashboard.putNumber("Target pitch", getPitch());
-        SmartDashboard.putNumber("Target floor distance", getFloorDistance());
-        SmartDashboard.putNumber("target id", getBestTarget() == null ? -1 : getBestTarget().getFiducialId());
-        SmartDashboard.putNumber("Pose ambiguity", getBestTarget() != null ? getBestTarget().getPoseAmbiguity() : -1);
-        //SmartDashboard.putNumber("target pitch", getBestTarget().getPitch());
     }
+
 }
 
 //potential Kalman implementation: get a sequence of camera readings, run line
